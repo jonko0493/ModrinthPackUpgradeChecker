723b0c5974b230966435f8fb7987c0270f42b5b1
jonko0493 <jonko0493@protonmail.com>
2024-06-17 21:15:56 -0700

    Initial commit

---
+++ ModrinthPack.cs
@@ -0,0 +1,110 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ModrinthPackUpgradeChecker
+{
+    public class ModrinthPack
+    {
+        [JsonPropertyName("game")]
+        public string Game { get; set; }
+        [JsonPropertyName("formatVersion")]
+        public int FormatVersion { get; set; }
+        [JsonPropertyName("versionId")]
+        public string VersionId { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("summary")]
+        public string Summary { get; set; }
+        [JsonPropertyName("files")]
+        public ModrinthFile[] Files { get; set; }
+        [JsonPropertyName("dependencies")]
+        public ModrinthDependencies Dependencies { get; set; }
+    }
+
+    public class ModrinthFile
+    {
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+        [JsonPropertyName("hashes")]
+        public ModrinthHashes Hashes { get; set; }
+        [JsonPropertyName("env")]
+        public ModrinthEnv Env { get; set; }
+        [JsonPropertyName("downloads")]
+        public string[] Downloads { get; set; }
+        [JsonPropertyName("fileSize")]
+        public int FileSize { get; set; }
+    }
+
+    public struct ModrinthHashes
+    {
+        [JsonPropertyName("sha512")]
+        public string Sha512 { get; set; }
+        [JsonPropertyName("sha1")]
+        public string Sha1 { get; set; }
+    }
+
+    public struct ModrinthEnv
+    {
+        [JsonPropertyName("client")]
+        public string Client { get; set; }
+        [JsonPropertyName("server")]
+        public string Server { get; set; }
+    }
+
+    public struct ModrinthDependencies
+    {
+        [JsonPropertyName("fabric-loader")]
+        public string FabricLoaderVersion { get; set; }
+        [JsonPropertyName("minecraft")]
+        public string MinecraftVersion { get; set; }
+    }
+
+    public struct SemanticVersion : IComparable
+    {
+        public int MajorVersion { get; set; }
+        public int MinorVersion { get; set; }
+        public int? BuildVersion { get; set; }
+
+        public static SemanticVersion Parse(string version)
+        {
+            string[] versions = version.Split('.');
+            SemanticVersion semVer = new();
+            try
+            {
+                semVer.MajorVersion = int.Parse(versions[0]);
+                semVer.MinorVersion = int.Parse(versions[1]);
+                if (versions.Length > 2)
+                {
+                    semVer.BuildVersion = int.Parse(versions[2]);
+                }
+                return semVer;
+            }
+            catch
+            {
+                return new() { MajorVersion = 0, MinorVersion = 0, BuildVersion = 0 };
+            }
+        }
+
+        public override string ToString()
+        {
+            return BuildVersion is not null ? $"{MajorVersion}.{MinorVersion}.{BuildVersion}" : $"{MajorVersion}.{MinorVersion}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is not null)
+            {
+                SemanticVersion otherVersion = (SemanticVersion)obj;
+                if (MajorVersion == otherVersion.MajorVersion)
+                {
+                    if (MinorVersion == otherVersion.MinorVersion)
+                    {
+                        return (BuildVersion ?? 0) - (otherVersion.BuildVersion ?? 0);
+                    }
+                    return MinorVersion - otherVersion.MinorVersion;
+                }
+                return MajorVersion - otherVersion.MajorVersion;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using Modrinth;
+using Modrinth.Models;
+using Mono.Options;
+using System.IO.Compression;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace ModrinthPackUpgradeChecker
+{
+    internal partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        public async static Task MainAsync(string[] args)
+        {
+            string mrpackPath = "", mcVersion = "";
+            bool inclusive = false;
+            OptionSet options = new()
+            {
+                { "p|m|pack|mrpack=", "Modrinth modpack to analyze", p => mrpackPath = p },
+                { "v|version=", "Minecraft version to check to upgrade to; blank to get the latest available version of each mod", v => mcVersion = v },
+                { "i|inclusive", "If set, will show mods that have the specified version rather than mods that don't", i => inclusive = true },
+            };
+            options.Parse(args);
+
+            Console.WriteLine($"Parsing pack '{mrpackPath}' and looking for {(string.IsNullOrEmpty(mcVersion) ? "latest version" : mcVersion)}...");
+
+            ZipArchive zip = ZipFile.OpenRead(mrpackPath);
+            string indexJson = new StreamReader(zip.Entries.First(f => f.Name == "modrinth.index.json").Open()).ReadToEnd();
+            ModrinthPack mrpack = JsonSerializer.Deserialize<ModrinthPack>(indexJson) ?? new();
+
+            Console.WriteLine($"Found {mrpack.Files.Length} mods. Fetching details from modrinth...");
+
+            ModrinthClient client = new();
+            Regex slugRegex = SlugRegex();
+
+            async Task<(string, string, string[])> GetModSupportedVersions(string cdnUri)
+            {
+                Project mod = await client.Project.GetAsync(slugRegex.Match(cdnUri).Groups["slug"].Value);
+                return (mod.Title, mod.Slug, mod.GameVersions);
+            }
+
+            IEnumerable<Task<(string, string, string[])>> gameVersionFetches = mrpack.Files.Select(m => GetModSupportedVersions(m.Downloads.First()));
+            (string, string, string[])[] modGameVersions = await Task.WhenAll(gameVersionFetches);
+
+            int compatibleMods = 0;
+            Console.WriteLine();
+
+            foreach ((string title, string slug, string[] gameVersions) in modGameVersions.OrderBy(m => m))
+            {
+                if (string.IsNullOrEmpty(mcVersion))
+                {
+                    Console.WriteLine($"{title} ({slug}): {gameVersions.OrderByDescending(v => v).First()}");
+                }
+                else
+                {
+                    if (gameVersions.Contains(mcVersion) && inclusive)
+                    {
+                        Console.WriteLine($"{title} ({slug})");
+                    }
+                    else if (!gameVersions.Contains(mcVersion) && !inclusive)
+                    {
+                        Console.WriteLine($"{title} ({slug}): {gameVersions.OrderByDescending(v => v).First()}");
+                    }
+                    
+                    if (gameVersions.Contains(mcVersion))
+                    {
+                        compatibleMods++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            if (!string.IsNullOrEmpty(mcVersion))
+            {
+                if (inclusive)
+                {
+                    Console.WriteLine($"{compatibleMods} compatible mods found ({mrpack.Files.Length - compatibleMods} mods incompatible)");
+                }
+                else
+                {
+                    Console.WriteLine($"{mrpack.Files.Length - compatibleMods} incompatible mods found ({compatibleMods} mods compatible)");
+                }
+            }
+        }
+
+        [GeneratedRegex(@"https:\/\/cdn\.modrinth\.com\/data\/(?<slug>[\w\d]+)\/")]
+        private static partial Regex SlugRegex();
+    }
+}
